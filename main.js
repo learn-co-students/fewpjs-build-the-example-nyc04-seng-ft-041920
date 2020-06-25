9e5bd6792a50cd37b61ebfcbf6b3db2faf6537c8
aleksarad <alexa.radevich1@gmail.com>
2020-06-25 16:58:59 -0400

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+
+const postContainers = document.querySelectorAll(".media-post");
+
+postContainers.forEach(function(post) {
+  let likeGlyph = post.querySelector(".like-glyph");
+  likeGlyph.addEventListener("click", function(e) {
+    mimicServerCall()
+    .then(function(){
+      processLike(e.target)
+    })
+    .catch(function(error) {
+      displayModalError(error);
+    })
+  })
+})
+
+//Helpers
+function displayModalError(error) {
+  const modalMessage = document.querySelector("#modal-message");
+  const modal = document.querySelector("#modal")
+  modal.classList.remove("hidden");
+  modalMessage.textContent = error;
+}
+
+function processLike(heart) {
+  if (heart.innerHTML === FULL_HEART) {
+    heart.innerHTML = EMPTY_HEART;
+    heart.style.color = "";
+  }
+  else {
+    heart.innerHTML = FULL_HEART;
+    heart.style.color = "red";
+  }
+}
+
 
 
 
