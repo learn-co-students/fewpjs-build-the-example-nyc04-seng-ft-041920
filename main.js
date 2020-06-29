505963a2415840185736fcb0da70c24096cb618c
jukebox101 <leonora.squires@gmail.com>
2020-06-29 00:34:48 -0400

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorHandler = document.querySelector("#modal")
+const likeHeart = document.querySelectorAll(".like-glyph")
 
+errorHandler.classList = "hidden"
 
+likeHeart.forEach(element => {
+  element.addEventListener("click", (e) => {
+    let heart = e.target
+  mimicServerCall()
+  .then(function(response){
+    if (heart.innerText == EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.className += " activated-heart"
+
+    }
+    else{
+      heart.innerText = EMPTY_HEART
+      heart.className = "like-glyph"
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+    document.querySelector("#modal").className = "";
+    errorHandler.innerText = error
+    setTimeout(function(){
+      document.querySelector("#modal").className = "hidden";
+    }, 5000)
+  })
+  })
+})
 
 
 //------------------------------------------------------------------------------
